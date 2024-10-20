3363c36f68a4899316a6ee62be9546e2281bef39
Gabo <51181002+gabrielgscr@users.noreply.github.com>
2024-10-20 11:01:11 -0600

    Se completa el código para la rotación de Pocoyo en Android

--- androidnativo/app/src/main/java/cr/ac/cuc/pocoyorotator/MainActivity.kt
+++ androidnativo/app/src/main/java/cr/ac/cuc/pocoyorotator/MainActivity.kt
@@ -1,10 +1,14 @@
 package cr.ac.cuc.pocoyorotator
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val rotateAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.rotate_animation)
+        val imgPocoyo : ImageView = findViewById(R.id.imgPocoyo)
+
+        // Iniciar la animación en el ImageView
+        imgPocoyo.startAnimation(rotateAnimation)
     }
 }
